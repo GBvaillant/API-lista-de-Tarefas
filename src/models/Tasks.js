7d41594dfec28d97d1c43846c24ea995612f04d4
GBvaillant <vaillantgabriel@gmail.com>
2023-07-26 20:19:09 -0300

    Mudança models

--- src/models/Tasks.js
+++ src/models/Tasks.js
@@ -4,15 +4,15 @@ const Schema = mongoose.Schema
 const task = new Schema({
     text: {
         type: String,
-        
+        required: true
     },
     category: {
         type: String,
-        
+        required: true
     },
     complete: {
         type: Boolean,
-        
+        required: true
     }
 })
 
